f9c27ba5a3969c3d3241dd8824545813be662f7b
matsu_web <rm.57.boc@gmail.com>
2024-08-19 10:36:54 +0900

    日付の表記を動的に

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,11 @@ class Article < ApplicationRecord
   validates :title, presence: true
   # validates titleの入力チェック presence(入力されているか)
   validates :content, presence: true
+
+  def display_created_at
+    # 日時の表示変更
+    I18n.l(self.created_at, format: :default)
+    # I18n.l(article.created_at, format: :default)
+    # articleの部分はselfで受け取れる
+  end
 end
