9ef00761b64157d0cb47a0176a041aca3eda6e8f
matsu_web <rm.57.boc@gmail.com>
2024-08-26 15:28:20 +0900

    updateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,4 +12,28 @@ class ProfilesController < ApplicationController
     @profile = current_user.build_profile
     # has_oneで定義してあるのでbuildしたいときbuild_profileと書く
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新完了'
+    else
+      flash.now[:error] = 'プロフィール更新に失敗しました'
+      # flashのkeyにerrorを入れvalueに'保存に失敗しました'を入れる
+      render :edit
+      # new.html.erbを表示
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
+  end
+
 end
