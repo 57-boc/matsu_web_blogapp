c57512903583a8758225e6431d5a3f4c517a9486
matsu_web <rm.57.boc@gmail.com>
2024-09-01 20:50:18 +0900

    AWSの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,10 +30,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-<<<<<<< HEAD
-=======
+  # amazonでちゃんと動くのか確認する
   # config.active_storage.service = :amazon
->>>>>>> origin/master
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
