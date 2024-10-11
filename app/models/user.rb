3d34e514542756de9b43ace54b7272ae85362e06
matsu_web <rm.57.boc@gmail.com>
2024-10-11 09:59:44 +0900

    アンフォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,16 +85,13 @@ class User < ApplicationRecord
 
   def follow!(user)
     # follow!で！が付いているのは例外処理があることを示している
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
+    user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = following_relationships.find_by!(following_id: user.id)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -112,6 +109,15 @@ class User < ApplicationRecord
       profile.avatar
     else
       'default-avatar.png'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
     end
   end
 end
