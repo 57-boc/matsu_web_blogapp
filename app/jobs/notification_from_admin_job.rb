984314fb55907dde02305d6d94f191064a414d54
matsu_web <rm.57.boc@gmail.com>
2024-10-23 17:29:14 +0900

    全ユーザにメールを送信できるように

--- app/jobs/notification_from_admin_job.rb
+++ app/jobs/notification_from_admin_job.rb
@@ -1,9 +1,10 @@
 class NotificationFromAdminJob <  ApplicationJob
   queue_as :default
 
-  def perform
+  def perform(msg)
     # Jobを作成したら必ずperformを書く
-
-
+    User.all.each do |user|
+      NotificationFromAdminMailer.notify(user,msg).deliver_later
+    end
   end
 end
