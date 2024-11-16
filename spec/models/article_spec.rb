aded28f9bfd60630d4290677013ab2acd99aa3bd
matsu_web <rm.57.boc@gmail.com>
2024-11-16 16:16:08 +0900

    let

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,18 +3,34 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   context 'タイトルを内容が入力されている場合' do
   # contextは前提条件
-    before do
-      user = User.create!({
+    let!(:user) do
+      User.create!({
         email: 'test@example.com',
         password: 'password'
       })
-      @article = user.articles.build({
+    end
+
+    let!(:article) do
+      user.articles.build({
         title: Faker::Lorem.characters(number: 10),
         content: Faker::Lorem.characters(number: 300)
       })
     end
+
+    # let!を使うときはbeforeを使用しない
+    # before do
+    #   user = User.create!({
+    #     email: 'test@example.com',
+    #     password: 'password'
+    #   })
+    #   @article = user.articles.build({
+    #     title: Faker::Lorem.characters(number: 10),
+    #     content: Faker::Lorem.characters(number: 300)
+    #   })
+    # end
+
     it '記事を保存できる' do
-      expect(@article).to be_valid
+      expect(article).to be_valid
       # be_validは有効である（保存できている）
     end
   end
