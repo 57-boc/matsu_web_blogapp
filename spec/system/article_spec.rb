2d16eb87052ad10794efe4bba1e3c6731446b473
matsu_web <rm.57.boc@gmail.com>
2024-11-17 21:31:27 +0900

    記事一覧のタイトルが表示されているか確認

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Article', type: :system do
+  let!(:user) { create(:user) }
+  let!(:article) {create_list(:article, 3, user: user) }
+
   it '記事一覧がひょうじされる' do
     visit root_path
+
+    article.each do |article|
+      expect(page).to have_content(article.title)
+    end
   end
 end
